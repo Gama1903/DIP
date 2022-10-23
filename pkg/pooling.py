4be8bb9c95dcd194502600f97658eff542d2343d
Gama1903 <1776029095@qq.com>
2022-10-23 13:19:00 +0800

    Add pooling.py

---
+++ pkg/pooling.py
@@ -0,0 +1,101 @@
+import numpy as np
+import cv2 as cv
+from typing import Callable
+
+from repo.pkg.read_show_rgb2gray import imread_m, imshow_m
+
+__all__ = [
+    "dec_pooling",
+    "pooling",
+]
+
+# 最大池化函数
+max_m = np.max
+# 均值池化函数
+average_m = lambda arr: int(np.average(arr))
+
+
+def dec_pooling(pooling: Callable):
+    """pooling函数的装饰器
+
+    """
+    def wrapper_func(InputMat: np.ndarray,
+                     size: int,
+                     step: int,
+                     func: Callable = max_m) -> np.ndarray:
+
+        if func == max_m:
+            return pooling(InputMat, size, step, max_m)
+        elif func == average_m:
+            return pooling(InputMat, size, step, average_m)
+        else:
+            raise ValueError(
+                "'{0}' is not a valid value; supported values are max_m and average_m. Defaults to max_m"
+                .format(func))
+
+    return wrapper_func
+
+
+@dec_pooling
+def pooling(InputMat: np.ndarray,
+            size: int,
+            step: int,
+            func: Callable = max_m) -> np.ndarray:
+    """池化
+
+    Args:
+        InputMat (np.ndarray): 输入矩阵, 必须为方形矩阵
+        size (int): 池化块的尺寸
+        step (int): 池化块的移动步长
+        func (Callable, optional): 池化方法. max_m为最大池化, average_m为均值池化. Defaults to max_m
+
+    Raises:
+        ValueError: 输入矩阵必须为方形矩阵
+
+    Returns:
+        np.ndarray: 输出矩阵
+    """
+    if InputMat.shape[0] != InputMat.shape[1]:
+        raise ValueError("InputMat must be a square matrix")
+
+    InputMat = np.array(InputMat)
+    # 计算输出矩阵的形状
+    OutputMat_shape = (InputMat.shape[0] - size) // step + 1
+    # 初始化输出矩阵
+    OutputMat = np.zeros((OutputMat_shape, OutputMat_shape))
+
+    i = 0
+    j = 0
+    out_i = 0
+    out_j = 0
+    while i < InputMat.shape[0]:
+        if i + size > InputMat.shape[0]:
+            break
+        while j < InputMat.shape[1]:
+            if j + size > InputMat.shape[1]:
+                break
+            OutputMat[out_i][out_j] = func(InputMat[i:i + size, j:j + size])
+            j += step
+            out_j += 1
+        i += step
+        out_i += 1
+        j = 0
+        out_j = 0
+
+    return OutputMat.astype("uint8")
+
+
+# 测试程序
+def main():
+    img_orig = imread_m("repo\images\cameraman_orig.tif")
+    print(img_orig.shape)
+    img_pooled = pooling(img_orig, 11, 11)
+    imshow_m((img_orig, img_pooled), ("Original Image", "Pooled Image"),
+             ("gray", "gray"), 1, 2)
+    # cv.imshow("Original Image", img_orig)
+    # cv.imshow("Pooled Image", img_pooled)
+    # cv.waitKey()
+
+
+if __name__ == "__main__":
+    main()
