e7ce5586738402b3a69e2e33f950d2a12a6e2e68
Gama1903 <1776029095@qq.com>
2022-10-27 07:27:42 +0800

    Refine C_intensityTrans.py

--- packages/C_intensityTrans.py
+++ packages/C_intensityTrans.py
@@ -1,3 +1,4 @@
+from re import I
 import cv2 as cv
 import numpy as np
 
@@ -19,40 +20,38 @@ def getNegative(img: np.ndarray, L: int = 256) -> np.ndarray:
         np.ndarray: 输出图像，图像底片
     """
 
-    return L - 1 - np.copy(img)
+    return L - 1 - img
 
 
-def logTrans(img: np.ndarray) -> np.ndarray:
+def logTrans(img: np.ndarray, c: int = 1) -> np.ndarray:
     """对数变换
 
     Args:
         img (np.ndarray): 输入图像
+        c (int, optional): 系数. Defaults to 1.
 
     Returns:
         np.ndarray: 输出图像
     """
+    maxVal = np.max(img)
 
-    c = 255 // np.log(1 + np.abs(np.max(img)))
-    a = np.log(1 + np.copy(img))
-
-    return 1 * np.log(1 + np.copy(img))
+    return (c * maxVal * np.log(1 + np.divide(img, maxVal))).astype("uint8")
 
 
-def gammaTrans(img: np.ndarray, gamma: float) -> np.ndarray:
+def gammaTrans(img: np.ndarray, gamma: float, c: int = 1) -> np.ndarray:
     """伽马变换
 
     Args:
         img (np.ndarray): 输入图像
         gamma (float): 大于1灰度压缩，小于1灰度扩展
+        c (int, optional): 系数. Defaults to 1.
 
     Returns:
         np.ndarray: 输出图像
     """
+    maxVal = np.max(img)
 
-    img = np.power(np.copy(img), gamma)
-    c = 255 // np.log(1 + np.abs(np.max(img)))
-
-    return 1 * img
+    return (c * maxVal * np.divide(img, maxVal)**gamma).astype("uint8")
 
 
 def main():
@@ -65,16 +64,20 @@ def main():
         cv.imshow("Image Negative", img_negative)
     # logtrans()测试程序
     elif flag_test == 1:
-        img_orig = cv.imread("images\\DFT_no_log.tif")
+        img_orig = cv.imread("images\\lena_rgb_orig.png", flags=0)
         img_processed = logTrans(img_orig)
         cv.imshow("Original Image", img_orig)
-        # cv.imshow("processed Image", img_processed)
+        cv.imshow("processed Image", img_processed)
     # gammatrans()的测试程序
     elif flag_test == 2:
-        img_orig = cv.imread("images\\washed_out_aerial_image.tif", flags=0)
-        img_processed = gammaTrans(img_orig, 1.5)
-        cv.imshow("Original Image", img_orig)
-        cv.imshow("processed Image", img_processed)
+        img_orig_1 = cv.imread("images\\fractured_spine.tif", flags=0)
+        img_orig_2 = cv.imread("images\\washed_out_aerial_image.tif", flags=0)
+        img_processed_1 = gammaTrans(img_orig_1, 0.3)
+        img_processed_2 = gammaTrans(img_orig_2, 4)
+        cv.imshow("Original Image 1", img_orig_1)
+        cv.imshow("Processed Image 1", img_processed_1)
+        cv.imshow("Original Image 2", img_orig_2)
+        cv.imshow("Processed Image 2", img_processed_2)
 
     cv.waitKey()
     cv.destroyWindow()
